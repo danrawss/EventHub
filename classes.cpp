d5e9e93093bd84bd40e640a858df5a68682d9ce9
Kuntsevych Daniil <kuntsevychdaniil22@stud.ase.ro>
2023-11-23 16:33:27 +0200

    improved version

--- classes.cpp
+++ classes.cpp
@@ -6,10 +6,16 @@ using namespace std;
 class Venue {
     string location = "";
     int maxNoSeats = 0;
-    int noSeatsAvailiable = 0;
+    int noSeatsAvailable = 0;
     int noRows = 0;
+    int entranceNo = 0;
+    int maxNoEntrances = 0;
+    string venueType = "";
 public:
     void setLocation(string location) {
+        if (strlen(location.c_str()) == 0) {
+            throw runtime_error("Provide not an empty string.");
+        }
         this->location = location;
     }
 
@@ -30,38 +36,90 @@ public:
         return this->maxNoSeats;
     }
 
-    void setNoAvailiableSeats(int seats) {
+    void setNoSeatsAvailable(int seats) {
         if (seats < 0) {
             throw runtime_error("Provide a positive number.");
         } 
         else {
-            this->noSeatsAvailiable = seats;
+            this->noSeatsAvailable = seats;
         }
     }
 
     int getNoAvailiableSeats() {
-        return this->noSeatsAvailiable;
+        return this->noSeatsAvailable;
     }
+
     void setNoRows(int rows) {
-        this->noRows = rows;
+        if (rows <= 0) {
+            throw runtime_error("Provide a positive number.");
+        }
+        else {
+            this->noRows = rows;
+        }
     }
 
     int getNoRows() {
         return this->noRows;
     }
 
+    void setEntranceNo(int entrance) {
+        if (entrance <= 0 || entrance > this->maxNoEntrances) {
+            throw runtime_error("Provide a valid no of the entrance.");
+        }
+        else {
+            this->entranceNo = entrance;
+        }
+    }
+
+    int getEntranceNo() {
+        return this->entranceNo;
+    }
+
+    void setMaxNoEntrances(int mEntrance) {
+        if (mEntrance <= 0) {
+            throw runtime_error("Provide a valid max no of the entrances.");
+        }
+        else {
+            this->maxNoEntrances = mEntrance;
+        }
+    }
+
+    int getMaxNoEntrances() {
+        return this->maxNoEntrances;
+    }
+
+    void setVenueType(string type) {
+        if (strlen(type.c_str()) == 0) {
+            throw runtime_error("Provide not an empty string.");
+        }
+        this->venueType = type;
+    }
+
+    string getVenueType() {
+        return this->venueType;
+    }
+
     Venue() {
         this->setLocation("Unknown");
         this->setNoRows(1);
         this->setMaxNoSeats(1);
-        this->setNoAvailiableSeats(1);
+        this->setNoSeatsAvailable(1);
+        this->setMaxNoEntrances(1);
+        this->setEntranceNo(1);
+        this->setVenueType("Unknown");
     }
 
-    Venue(string location, int maxNoSeats, int noRows) : location(location), maxNoSeats(maxNoSeats), noRows(noRows) {
-        this->setLocation(location);
-        this->setMaxNoSeats(maxNoSeats);
-        this->setNoAvailiableSeats(maxNoSeats);
-        this->setNoRows(noRows);
+    Venue(string location, int maxNoSeats, int noRows, int noSeatsAvailable, int entranceNo, int maxNoEntrances, string venueType)
+        : location(location), 
+          maxNoSeats(maxNoSeats), 
+          noRows(noRows),
+          noSeatsAvailable(noSeatsAvailable),
+          entranceNo(entranceNo),
+          maxNoEntrances(maxNoEntrances),
+          venueType(venueType) {
+        if (maxNoSeats <= 0 || noSeatsAvailable < 0 || noRows <= 0 || entranceNo < 0 || maxNoEntrances <= 0) {
+            throw std::runtime_error("Invalid input. Ensure positive values for maximum seats, available seats, rows, entrance number, and maximum entrances.");
+        }
     }
     ~Venue() {
 
@@ -70,36 +128,72 @@ public:
 
 
 class Event {
-    char date[11] = "";
-    char* name = nullptr;
-    string time = "";
+    char eventDate[11] = "";      // dd/mm/yyyy
+    char* eventName = nullptr;
+    string eventTime = "";        // hh:mm:ss
+    string eventType = "";
+    int eventDuration = 0;   //in minutes
+    static int TOTAL_EVENTS;
 public:
-    void setDate(char date[11]) {
-
+    static int getTotalEvents() {
+        return Event::TOTAL_EVENTS;
     }
 
+    void setDate(const char* newDate) {
+        if (strlen(newDate) != 10) {
+            throw runtime_error("Wrong date.");
+        }
+        if (newDate[2] != '/' || newDate[5] != '/') {
+            throw runtime_error("Wrong date format.");
+        } 
+        strncpy(this->eventDate, newDate, 11);
+    }   
+
     char* getDate() {
-        char* dateCopy = new char[strlen(this->date) + 1];
-		strncpy(dateCopy, this->date, strlen(this->date) + 1);
+        char* dateCopy = new char[11];
+		strncpy(dateCopy, this->eventDate, 11);
 		return dateCopy;
     }
 
-    void setName(char* name) {
-        delete[] this->name;
+    void setName(char* newName) {
+        delete[] this->eventName;
+        if (strlen(newName) == 0) {
+            throw runtime_error("Provide a name, not an empty string.");
+        }
+        strncpy(this->eventName, newName, strlen(newName) + 1);
     }
 
     char* getName() {
-        char* nameCopy = new char[strlen(this->name) + 1];
-		strncpy(nameCopy, this->name, strlen(this->name) + 1);
+        char* nameCopy = new char[strlen(this->eventName) + 1];
+		strncpy(nameCopy, this->eventName, strlen(this->eventName) + 1);
 		return nameCopy;
     }
 
-    void setTime(string time) {
-
+    void setEventTime(string newTime) {
+        if (strlen(newTime.c_str()) != 8) {
+            throw runtime_error("Wrong time.");
+        }
+        if (newTime[2] != ':' || newTime[5] != ':') {
+            throw runtime_error("Wrong time format.");
+        }
+        this->eventTime = newTime;
     }
 
-    string getTime() {
-        return this->time;
+    string getEventTime() {
+        return this->eventTime;
+    }
+
+    void setEventDuration(int newDuration) {
+        if (newDuration <= 0) {
+            throw runtime_error("Provide a positive number.");
+        }
+        else {
+            this->eventDuration = newDuration;
+        }
+    }
+
+    int getEventDuration() {
+        return this->eventDuration;
     }
 
     Event() {
@@ -111,12 +205,97 @@ public:
     }
 };
 
+int Event::TOTAL_EVENTS = 0;
+
 class Ticket {
+    const int ticketId = 0;
+    string ticketType = "";
+    double ticketPrice = 0;
+    string ticketHolderName = "";
+    char purchaseDate[11] = "";
+    string purchaseTime = "";
+    bool isValid = false;
+public:
+    int getTicketId() {
+        return this->ticketId;
+    }
 
-};
+    void setTicketType(string type) {
+        if (strlen(type.c_str()) == 0) {
+            throw runtime_error("Please provide correct ticket type");
+        }
+        else {
+            this->ticketType = type;
+        }
+    }
 
-class Menu {
+    string getTicketType() {
+        return this->ticketType;
+    }
 
+    void setTicketPrice(double price) {
+        if (price <= 0) {
+            throw runtime_error("Provide a positive value for the ticket");
+        }
+        else {
+            this->ticketPrice = price;
+        }
+    }
+
+    double getTicketPrice() {
+        return this->ticketPrice;
+    }
+
+    void setTicketHolderName(string name) {
+        if (strlen(name.c_str()) == 0) {
+            throw runtime_error("Provide a name, not an empty string.");
+        }
+        else {
+            this->ticketHolderName = name;
+        }
+    }
+
+    string getTicketHolderName() {
+        return this->ticketHolderName;
+    }
+
+    void setPurchaseDate(const char* newDate) {
+        if (strlen(newDate) != 10) {
+            throw runtime_error("Wrong date.");
+        }
+        if (newDate[2] != '/' || newDate[5] != '/') {
+            throw runtime_error("Wrong date format.");
+        }
+        strncpy(this->purchaseDate, newDate, 11);
+    }
+
+    char* getPurchaseDate() {
+        char* copy = new char[11];
+        strncpy(copy, this->purchaseDate, 11);
+        return copy;
+    }
+
+    void setPurchaseTime(string newTime) {
+        if (strlen(newTime.c_str()) != 8) {
+            throw runtime_error("Wrong time.");
+        }
+        if (newTime[2] != ':' || newTime[5] != ':') {
+            throw runtime_error("Wrong time format.");
+        }
+        this->purchaseTime = newTime;
+    }
+
+    string getPurchaseTime() {
+        return this->purchaseTime;
+    }
+
+    void setIsValid (bool isValid) {
+        this->isValid = isValid;
+    }
+
+    bool getIsValid () {
+        return this->isValid;
+    }
 };
 
 int main() {
